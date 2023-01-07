74c86bde43d64f886f0d86338d9f1b58dbb91ff8
Glen Hobby <aussieguy@users.noreply.github.com>
2023-01-07 17:57:43 +1100

    Add files via upload

---
+++ SmartWalletV2.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract wallet {
+    address public walletOwner;
+
+    uint256 withdrawlAllowance;
+
+    mapping(address => uint256) accounts; //Used in withdrawFunds function.
+    mapping(address => uint256) allowances; //Used in setAnAllowance function.
+
+    constructor() {
+        walletOwner = msg.sender; //Set the wallet owner to who creates contract
+    }
+
+    function setAnAllowance(address payable _to, uint256 _allowance) public {
+        require(_to == msg.sender, "You are not the account holder!"); //Only the account address can set their allowance limit.
+        allowances[_to] = _allowance;
+    }
+
+    function withdrawFunds(
+        address payable _to,
+        address payable _from,
+        uint256 _amount
+    ) public {
+        require(
+            _from == msg.sender,
+            "Withdrawls not allowed unless you are the wallet owner."
+        ); //Check account owner.
+        require(_amount <= accounts[msg.sender], "Insufficient funds!"); //Check for funds.
+
+        if (allowances[msg.sender] > 0) {
+            //Check withdrawl allowance.
+            require(
+                _amount <= allowances[msg.sender],
+                "You are exceeding the allowance amount, aborting!"
+            );
+        }
+
+        accounts[msg.sender] -= _amount; //Place here before send to prevent reentrancy hack
+
+        bool sendStatus = payable(_to).send(_amount);
+        require(sendStatus, "Error in sending funds!"); //Error trap if send doesn't work
+    }
+
+    function changeOwner(address _newOwner) public {
+        require(walletOwner == msg.sender, "Must be wallet owner!");
+
+        walletOwner = _newOwner; //Set new wallet owner.
+    }
+
+    receive() external payable {
+        accounts[msg.sender] += msg.value;
+    }
+}
